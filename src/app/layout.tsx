354c05f59efd0e6f83e5db52a79484d04b09e750
cuongbq <erikbuidt@gmail.com>
2025-06-26 21:58:07 +0700

    Add favicon images and web manifest for improved site branding
    - Added favicon-16x16.png and favicon-32x32.png to enhance site identity in browser tabs and bookmarks.
    - Created site.webmanifest to define application metadata and icons for progressive web app support.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,9 @@ export const metadata: Metadata = {
 	},
 	publisher: "Tea Mi Pet Store",
 	robots: "index, follow",
+	icons: {
+		icon: "/favicon.ico",
+	},
 	keywords:
 		"pet store, cửa hàng thú cưng, spa chó mèo, khách sạn thú cưng, Tea Mi Pet Store, phụ kiện thú cưng Quận 7",
 	description:
@@ -57,7 +60,24 @@ export default function RootLayout({
 	return (
 		<html lang="vi">
 			<link rel="url" href="https://teamipetstore.com" />
-			<link rel="icon" href="/favicon.ico" sizes="any" />
+			<link
+				rel="apple-touch-icon"
+				sizes="180x180"
+				href="/apple-touch-icon.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="32x32"
+				href="/favicon-32x32.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="16x16"
+				href="/favicon-16x16.png"
+			/>
+			<link rel="manifest" href="/site.webmanifest" />
 			<body
 				className={`${nunito.variable} ${pacifico.variable} bg-white text-gray-800`}
 			>
